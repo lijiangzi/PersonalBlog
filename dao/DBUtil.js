a90b9d6ef2cb468399a8b917118355c5b1f8e941
lijiangzi <2206208402@qq.com>
2019-08-19 08:56:12 +0800

    个修改代码中数据的连接地址为虚拟机的ip

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host: "127.0.0.1",
+        host: "127.168.0.103",
         port: "3306",
         user: "root",
         password: "123456",
